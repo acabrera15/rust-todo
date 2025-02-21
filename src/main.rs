0bd028ccfd123f239c4deba44036de2a281e10ad
Andrew Cabrera <andrewcabrera@Mac.attlocal.net>
2025-02-20 20:21:36 -0800

    connected db
    Connected sqlite Db for basic control

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use rusqlite::{Connection, Result};
+
 fn main() {
-    println!("Hello, world!");
+    connection();
+    println!("testing");
+}
+
+fn connection() -> Result<()> {
+    let conn = Connection::open("todo.db")?;
+
+    conn.execute(
+        "CREATE TABLE todo (
+              id INTEGER PRIMARY KEY,
+              task VARCHAR(256) NOT NULL,
+              completed BOOLEAN NOT NULL DEFAULT FALSE
+        )",
+        []
+       )?;
+
+    Ok(())
 }
